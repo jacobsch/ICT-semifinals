276f08f9936e91fb1cc7f1bc0b8b31698db70436
Jacob Schwartz <jacobschwartz@live.ca>
2019-06-11 12:35:02 -0700

    Final Changes

--- paint-a-house/paintAHouse.java
+++ paint-a-house/paintAHouse.java
@@ -8,34 +8,45 @@ public class paintAHouse {
      */
 
 
-    public static double wall(){
+    public static double getWallArea(){
         Scanner prompt = new Scanner(System.in);
 
         // First wall
         System.out.println("What is the length of your first wall in meters?");
         while(!prompt.hasNextDouble()) {
-            System.out.println("Oops "+name+" please enter a number!");
+            System.out.println("Oops please enter a number!");
             prompt.next();
         }
         double length = prompt.nextDouble();
         System.out.println("What is the height of your first wall in meters?");
         while(!prompt.hasNextDouble()) {
-            System.out.println("Oops "+name+" please enter a number!");
+            System.out.println("Oops please enter a number!");
             prompt.next();
         }
         double height = prompt.nextDouble();
-        prompt.close();
 
         double area = length * height;
 
+        System.out.println();
         System.out.println("What is the area of your window in m^2?");
         System.out.println("If there are no windows enter 0");
         while(!prompt.hasNextDouble()) {
-            System.out.println("Oops "+name+" please enter a number!");
+            System.out.println("Oops please enter a number!");
             prompt.next();
         }
         double window = prompt.nextDouble();
 
+        System.out.println();
+        System.out.println("What is the area of your door in m^2?");
+        System.out.println("If there are no doors enter 0");
+        while(!prompt.hasNextDouble()) {
+            System.out.println("Oops please enter a number!");
+            prompt.next();
+        }
+        double door = prompt.nextDouble();
+
+        area = area - door;
+        prompt.close();
         return area;
     }
 
@@ -43,7 +54,8 @@ public class paintAHouse {
         String name;
         double areaToPaint;
         double vatougiosSplat = 20.0;
-        double totalArea;
+        double areaTotal;
+        String unit = "m2";
 
         // Intro and Name
         System.out.println("Welcome to the Vatougios Splat Paint Co.");
@@ -56,11 +68,47 @@ public class paintAHouse {
         System.out.println("");
         System.out.println(name+", Vatougios Splat only paints houses that have 4 walls that need to be painted.");
 
+        System.out.println();
         System.out.println("Wall #1!");
-        wall();
-        System.out.println(name+" the area for your wall is " + area + "m^2");
+        double areaOne = getWallArea();
+        System.out.print('\f');
+        System.out.println(name+" the area for your first wall is " + areaOne + unit);
 
-        Scanner bool = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Wall #2!");
+        double areaTwo = getWallArea();
+        areaTotal = areaOne + areaTwo;
+        System.out.println(name+" the area for your second wall is " + areaTwo + unit);
+        System.out.print('\f');
+        System.out.println("Your current total area is " + areaTotal + unit);
 
+        System.out.println();
+        System.out.println("Wall #3!");
+        double areaThree = getWallArea();
+        areaTotal = areaTotal + areaThree;
+        System.out.println(name+" the area for your third wall is " + areaThree + unit);
+        System.out.print('\f');
+        System.out.println("Your current total area is " + areaTotal + unit);
+
+        System.out.println();
+        System.out.println("Wall #4!");
+        double areaFour = getWallArea();
+        areaTotal = areaTotal + areaFour;
+        System.out.println(name+" the area for your fourth and final wall is " + areaFour + unit);
+        System.out.print('\f');
+        System.out.println("The total paintable surface is " + areaTotal + unit);
+        System.out.println();
+
+        System.out.println("Now lets do some calculations " + name + "!!");
+        double gallons = areaTotal/10;
+        gallons = Math.ceil(gallons);
+
+        System.out.println(name + ", your house requires just about " + gallons + " of paint to cover your house fully.");
+        double paintPrice = gallons * 40;
+        paintPrice = paintPrice - 0.1;
+        System.out.println("Vatougios Splat will paint your house for an incredible deal of $40 per gallon");
+        System.out.println("bringing the total paint price to $" + paintPrice);
+        System.out.println();
+        System.out.println("Have a good day " +name +"!");
     }
 }
